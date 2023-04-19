f54216716a132047889c0dbbb7c876e70530b972
shinyoung hong <sy2667@naver.com>
2023-04-19 16:36:21 +0900

    비밀 정보 보호

---
+++ README.md
@@ -0,0 +1,1 @@
+# node-plate-1
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     maxlength: 50,
@@ -29,4 +29,4 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
-module.export = { User };
+module.exports = { User };
