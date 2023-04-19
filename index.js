766e4e0ec1124e0a8cd9f44261d171acbdf1e444
shinyoung hong <sy2667@naver.com>
2023-04-19 10:11:13 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(
+    "mongodb+srv://hongsi:hongsi@nodeplate.lebaunl.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB Connected...."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log("Example app listening on port ${port}!"));
